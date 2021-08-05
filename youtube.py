eaf30c99e8ed58afdbd91af4c375bbf5fa79adec
sduro <sergiduro@gmail.com>
2021-08-05 20:29:26 +0200

    nueva configuracion

--- youtube.py
+++ youtube.py
@@ -1,14 +1,15 @@
 import youtube_dl
-import os
-from bottle import run, get, post, request, template # or route
+import json
+from bottle import run, get, post, request, template, route # or route
+
+with open("youtube.json", "r") as jsonfile:
+    data = json.load(jsonfile) # Reading the file
+    jsonfile.close()
 
 @get('/login') # or @route('/login')
 def login():
-
     return '''
-        <form action="/login" method="post">
             Youtube URL: <input name="URL" type="text" />
-            Video ID: <input name="username" type="text" />
             <label for="downloadoption">Option</label>
                 <select name="downloadoption" id="downloadoption">
                 <option value="mp3">Solo Audio</option>
@@ -20,40 +21,42 @@ def login():
 
 @post('/login') # or @route('/login', method='POST')
 def do_login():
+    yield "Downloading..."
     URL = request.forms.get('URL')
-    username = request.forms.get('username')
     downloadoption = request.forms.get('downloadoption')
-    print (downloadoption)
     #download_clip("https://www.youtube.com/watch?v="+username,downloadoption)
     download_clip(URL,downloadoption)
+    yield "Done"
     return '''<form action="/login" method="post">
                  <input value="Back" type="submit" />
         </form>
         '''
 
 def download_clip(url, name):
-    '''download video equal to MP4, only audio MP3'''
+    '''
+    Cambiar path para raspberry /home/pi/Documents/youtube
+    '''
     if name == "mp4":
         ydl_opts = {
             'format': 'bestvideo+bestaudio/best',
-            'outtmpl': '/home/sduro/Documentos/%(title)s-%(id)s.%(ext)s',#+name,
+            'outtmpl': data['download_dir']+'%(title)s-%(id)s.%(ext)s',#+name, #Cambiar path para raspberry
             'noplaylist': True,
             'continue_dl': True,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': '%(ext)s',
-                'preferredquality': '192', }]
+                'preferredquality': data['bitrate'], }]
         }
     else:
         ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': '/home/sduro/Documentos/%(title)s.mp3',#+name,
+        'outtmpl': data['download_dir']+'%(title)s.mp3',#+name,
         'noplaylist': True,
         'continue_dl': True,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '192', }]
+            'preferredquality': data['bitrate'], }]
     }
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -66,4 +69,4 @@ def download_clip(url, name):
         return False 
 
 #download_clip("https://www.youtube.com/watch?v=uGhKqb2Ow3E",True)
-run(host='0.0.0.0', port=8080, debug=True)
+run(host='localhost', port=8088, debug=True)
