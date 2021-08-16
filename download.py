98ff2aa53b25b987f2e646f72f1d0cbe00100128
Sergi Duro <Sergi.Duro@acer.com>
2021-08-16 16:54:50 +0200

    new commit

--- download.py
+++ download.py
@@ -1,31 +1,22 @@
 import youtube_dl
+
 def download_clip(url, name,data):
     '''
     path viene de fichero de configuracion
     '''
-    if name == "mp4":
-        ydl_opts = {
-            'format': 'bestvideo+bestaudio/best',
-            'outtmpl': data['download_dir']+'%(title)s-%(id)s.%(ext)s',#+name, #Cambiar path para raspberry
+    print (data[name])
+    ydl_opts = {
+            'format': data[name]['format'],
+            'outtmpl': data['configuration']['download_dir']+'%(title)s-%(id)s.%(ext)s',
             'noplaylist': True,
             'continue_dl': True,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': '%(ext)s',
-                'preferredquality': data['bitrate'], }]
-        }
-    else:
-        ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': data['download_dir']+'%(title)s.mp3',#+name,
-        'noplaylist': True,
-        'continue_dl': True,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': data['bitrate'], }]
+                'preferredcodec': data[name]['preferredcodec'],
+                'preferredquality': data[name]['preferredquality'], }]
     }
     try:
+        
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.cache.remove()
             info_dict = ydl.extract_info(url, download=True)
--- youtube.py
+++ youtube.py
@@ -2,9 +2,8 @@
 Pagina web para descargar mp3
 youtube.json ->fichero de configuracion
 '''
-import youtube_dl
 import json
-from bottle import run, get, post, request, template, route # or route
+from bottle import run, get, post, request, template, route,static_file, view # or route
 from download import download_clip
 
 with open("youtube.json", "r") as jsonfile:
@@ -13,17 +12,19 @@ with open("youtube.json", "r") as jsonfile:
 
 @get('/login') # or @route('/login')
 def login():
-    return '''
-            <form action="/login" method="post">
-            Youtube URL: <input name="URL" type="text" />
-            <label for="downloadoption">Option</label>
-                <select name="downloadoption" id="downloadoption">
-                <option value="mp3">Solo Audio</option>
-                <option value="mp4">Audio+Video</option>
-            </select> 
-            <input value="Download" type="submit" />
-        </form>
-    '''
+   return static_file('index.html',root='template/')
+   # return '''
+   #         <form action="/login" method="post">
+   #         Youtube URL: <input name="URL" type="text" />
+   #         <label for="downloadoption">Option</label>
+   #             <select name="downloadoption" id="downloadoption">
+   #             <option value="mp3">Solo Audio</option>
+   #             <option value="mp4">Audio+Video</option>
+   #         </select> 
+   #         <input value="Download" type="submit" />
+   #     </form>
+   # '''
+    
 @post('/login') # or @route('/login', method='POST')
 def do_login():
     yield "Downloading..."
