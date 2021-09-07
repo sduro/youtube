496b46f56b1a225d325a7df9e391b5e204560f42
Sergi Duro <Sergi.Duro@acer.com>
2021-09-07 07:19:42 +0200

    updates in files

--- youtube.py
+++ youtube.py
@@ -3,6 +3,7 @@ Pagina web para descargar mp3
 youtube.json ->fichero de configuracion
 '''
 import json
+import os
 from bottle import run, get, post, request, template, route,static_file, view # or route
 from download import download_clip
 
@@ -10,20 +11,19 @@ with open("youtube.json", "r") as jsonfile:
     data = json.load(jsonfile) # Reading the file
     jsonfile.close()
 
+def config():
+    with open("youtube.json", "w") as jsonfile:
+        json.dump(data,jsonfile,ensure_ascii=False) # writing the file
+        jsonfile.close()
+
+@route("/static/css/<filename>")
+def server_static(filename):
+	return static_file(filename,root="./static/css")
+
 @get('/login') # or @route('/login')
 def login():
-   return static_file('index.html',root='template/')
-   # return '''
-   #         <form action="/login" method="post">
-   #         Youtube URL: <input name="URL" type="text" />
-   #         <label for="downloadoption">Option</label>
-   #             <select name="downloadoption" id="downloadoption">
-   #             <option value="mp3">Solo Audio</option>
-   #             <option value="mp4">Audio+Video</option>
-   #         </select> 
-   #         <input value="Download" type="submit" />
-   #     </form>
-   # '''
+    actual_path = os.getcwd()
+    return template('static/index.html',path=actual_path,download_path=data['configuration']['download_dir'])
     
 @post('/login') # or @route('/login', method='POST')
 def do_login():
@@ -32,8 +32,17 @@ def do_login():
     downloadoption = request.forms.get('downloadoption')
     download_clip(URL,downloadoption,data)
     yield "Done"
-    return '''<form action="/login" method="post">
-                 <input value="Back" type="submit" />
-        </form>
-        '''
+    return template('/static/index.html')
+
+@route('/config')
+def configuration():
+    return template('static/configuration.html')
+
+@post('/config')
+def configuration_save():
+    new_path = request.forms.get('new_path')
+    print (new_path)
+    data['configuration']['download_dir'] = new_path
+    return template('static/configuration.html')
+
 run(host='localhost', port=8088, debug=True)
